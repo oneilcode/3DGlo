6b13a971411bfb89ef20d4595cac320cb4fb982f
Vika Oneil <oneilcode111@gmail.com>
2023-01-15 19:57:02 +1000

    Добавлено усложненка - один обработчик событиый в функции меню, скрывающий само меню приклике на другую область

--- src/modules/toggleMenu.js
+++ src/modules/toggleMenu.js
@@ -1,20 +1,17 @@
 const toggleMenu = () => {
 
-   const menuBtn = document.querySelector('.menu');
    const menu = document.querySelector('menu');
 
    const handleMenu = () => {
       menu.classList.toggle('active-menu');
    };
 
-   menuBtn.addEventListener('click', handleMenu);
-
-   menu.addEventListener('click', (e) => {
-      if (e.target.closest('.menu') || !e.target.closest('menu')) {
+   document.body.addEventListener('click', (e) => {
+      if (menu.classList.contains('active-menu') && !e.target.closest('menu')) {
          handleMenu();
-      } else if (e.target.classList.contains('close-btn')) {
+      } else if (e.target.closest('.menu') || e.target.closest('.close-btn')) {
          handleMenu();
-      } else if (e.target.matches('ul>li>a')) {
+      } else if (e.target.closest('menu>ul>li>a')) {
          handleMenu();
       }
    });
